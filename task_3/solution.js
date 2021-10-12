33e19badf289d0a706a45828a6d5b85df9817e55
Вячеслав Волков <suorus@gmail.com>
2021-10-12 23:14:51 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -4,12 +4,13 @@ function renderCartItem(item) {
     // Задание №3.1. Формирование строки корзины
 
     // product - объект вида {id: id, title: title, count: count, price: price}
-    let item {id: 1, title: "Пицца", count: 5, price: "500.00"};
-    // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
+     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
     // title - наименование
     // price - цена
     // count - количество
+    
+     
 
     // сформируйте строку itemCountText вида:
     // количество × цена ₽ = стоимость ₽
@@ -17,23 +18,22 @@ function renderCartItem(item) {
     // 5 × 500.00 ₽ = 2500 ₽
     
     // 1. Создайте переменную itemCountText, равную пустой строке
-    let itemCountText = ' ';
+    
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText += count ;
+    
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
-    itemCountText += ` × ` ;
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText += price ;
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
-    itemCountText += ` ₽ = ` ;
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    let sum = count * price;
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
-    itemCountText += sum ;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
-    itemCountText += ` ₽`;
+    
+    let sum = item.count*item.price;
+    let itemCountText = `${item.count} × ${item.price} ₽ = ${sum} ₽`;
+    
 
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
+   
 }
